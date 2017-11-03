d9ac84b85e8032a636468d251c42f0dc5ca402a8
Christopher Smith <ctsmith2308@gmail.com>
2017-11-02 18:37:40 -0600

    render message list to app.js

---
+++ src/components/Messages.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Message from './Message'
+
+const Messages = ({messages}) => (
+
+<div className = "container">
+{messages.map(message => <Message key={message.id} message ={message}/>)}
+</div>
+
+)
+
+
+export default Messages
